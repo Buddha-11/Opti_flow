f8c66ef6c020732585d703115eed33a6c0ad46f0
Snehalgupta-07 <guptasnehal877@gmail.com>
2024-11-14 06:30:25 +0530

    CSS

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,26 @@
 import '../index.css'
 import Navbar from '../components/Navbar'
 import TaskList from '../components/Task'
+import Image from '../images/Home_Image.png'
 
-const Home=()=>{
+const Home = () => {
     return (
         <div className="home">
-            <div className="Navbar">
-                <Navbar />
+            <Navbar />
+            <div className="spacer3"></div>
+            
+            <div className="content">
+                <div className="text">
+                    <h1>Optimize your workflow with <strong>OptiFlow</strong></h1>
+                </div>
+                <div className="image">
+                    <img src={Image} alt="Workflow optimization" />
+                </div>
             </div>
+            
             <div className="Task">
                 <TaskList />
             </div>
-
-            
         </div>
     )
 }
